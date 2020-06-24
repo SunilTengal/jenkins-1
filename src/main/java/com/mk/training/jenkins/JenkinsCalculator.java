8133eb09d1b729e8f1faae0cf104b02e91083d52
root <root@ip-172-31-62-0.ec2.internal>
2020-06-24 12:21:16 +0000

    Commit class

--- src/main/java/com/mk/training/jenkins/JenkinsCalculator.java
+++ src/main/java/com/mk/training/jenkins/JenkinsCalculator.java
@@ -10,5 +10,8 @@ public class JenkinsCalculator {
 	public int subtractNumbers(int numberOne, int numberTwo) {
 		return numberOne - numberTwo;
 	}
-
+        public static void main(String[] args) {
+        JenkinsCalculator objJenkinsCalculator = new  JenkinsCalculator();
+          System.out.println(objJenkinsCalculator.addNumbers(3,4)); 
 }
+} 
